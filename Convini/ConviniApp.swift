bae10d35bdbdfb152455636dbeb8ef25e1fcf415
mm083434 <mm083434@naver.com>
2026-02-27 16:29:42 +0900

    Initial Commit

---
+++ Convini/ConviniApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConviniApp.swift
+//  Convini
+//
+//  Created by Hyunsoo Lee on 2/27/26.
+//
+
+import SwiftUI
+
+@main
+struct ConviniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
