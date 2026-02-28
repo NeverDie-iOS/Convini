1e7ff37e7491a2cd3caedc941061ff8c19936021
mm083434 <mm083434@naver.com>
2026-02-28 21:26:39 +0900

    Reorganize project structure

--- Convini/App/ConviniApp.swift
+++ Convini/App/ConviniApp.swift
@@ -1,17 +1,10 @@
-//
-//  ConviniApp.swift
-//  Convini
-//
-//  Created by Hyunsoo Lee on 2/27/26.
-//
-
 import SwiftUI
 
 @main
 struct ConviniApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainTabView()
         }
     }
 }
